4ac6bc28eb0673a91e54dbacfc8abd08129f3888
hejie <hejie.zhang@hotmail.com>
2025-08-25 15:34:30 -0500

    Fixed the issue of storing plain text after changing password

--- src/main/java/com/hejie/store/users/UserService.java
+++ src/main/java/com/hejie/store/users/UserService.java
@@ -63,7 +63,7 @@ public class UserService {
             throw new AccessDeniedException("Password does not match");
         }
 
-        user.setPassword(request.getNewPassword());
+        user.setPassword(passwordEncoder.encode(request.getNewPassword()));
         userRepository.save(user);
     }
 }
